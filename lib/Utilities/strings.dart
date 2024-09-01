c23494e1c7af04291e062cb0861be4051ca2cfaa
Sahil Agrawal <109281098+SahilAgr10@users.noreply.github.com>
2024-09-01 23:08:02 +0530

    Home Ui update

---
+++ lib/Utilities/strings.dart
@@ -0,0 +1,18 @@
+class MyString {
+  static const String mainTitle = "Planned Trips";
+  static const String deletedTrip = "This Trip is deleted";
+  static const String doneAllTask = "You Have Done All Trips!👌";
+  static const String addNewTask = "Add New Destination 🌍";
+  static const String updateCurrentTask = "Update ";
+  static const String taskString = "Task";
+  static const String titleOfTitleTextField = "What are you planing😇?";
+  static const String addNote = 'Add Your Preferred Destination';
+  static const String timeString = "Time";
+  static const String startDateString = "Start Date ";
+  static const String endDateString = "End Date ";
+  static const String deleteTask = "Delete Trip";
+  static const String addTaskString = "Add Trip";
+  static const String updateTaskString = "Update Trip";
+  static const String oopsMsg = "Oops!";
+  static const String areYouSure = "Are You Sure?";
+}
